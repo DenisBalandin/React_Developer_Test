8664e6ac7f1eb85983122dfecae5cd304695358f
Denis Balandin <temhbin@bk.ru>
2020-09-03 14:58:50 -0400

    Update server.js

--- SeverApi/src/server.js
+++ SeverApi/src/server.js
@@ -9,23 +9,16 @@ const userdb = JSON.parse(fs.readFileSync('./users.json', 'UTF-8'))
 
 server.use(bodyParser.urlencoded({extended: true}))
 server.use(bodyParser.json())
-server.use(jsonServer.defaults());
 
 const SECRET_KEY = '123456789'
 
 const expiresIn = '1h'
-
-// Create a token from a payload 
 function createToken(payload){
   return jwt.sign(payload, SECRET_KEY, {expiresIn})
 }
-
-// Verify the token 
 function verifyToken(token){
   return  jwt.verify(token, SECRET_KEY, (err, decode) => decode !== undefined ?  decode : err)
 }
-
-// Check if the user exists in database
 function isAuthenticated({email, password}){
   return userdb.users.findIndex(user => user.email === email && user.password === password) !== -1
 }
@@ -42,7 +35,6 @@ server.post('/auth/register', (req, res) => {
     res.status(status).json({status, message});
     return
   }
-
 fs.readFile("./users.json", (err, data) => {  
     if (err) {
       const status = 401
@@ -50,16 +42,12 @@ fs.readFile("./users.json", (err, data) => {
       res.status(status).json({status, message})
       return
     };
-
-    // Get current users data
     var data = JSON.parse(data.toString());
-
-    // Get the id of last user
     var last_item_id = data.users[data.users.length-1].id;
 
     //Add new user
-    data.users.push({id: last_item_id + 1, email: email, password: password}); //add some data
-    var writeData = fs.writeFile("./users.json", JSON.stringify(data), (err, result) => {  // WRITE
+    data.users.push({id: last_item_id + 1, email: email, password: password}); 
+    var writeData = fs.writeFile("./users.json", JSON.stringify(data), (err, result) => { 
         if (err) {
           const status = 401
           const message = err
@@ -74,8 +62,6 @@ fs.readFile("./users.json", (err, data) => {
   console.log("Access Token:" + access_token);
   res.status(200).json({access_token})
 })
-
-// Login to one of the users from ./users.json
 server.post('/auth/login', (req, res) => {
   console.log("login endpoint called; request body:");
   console.log(req.body);
@@ -90,33 +76,6 @@ server.post('/auth/login', (req, res) => {
   console.log("Access Token:" + access_token);
   res.status(200).json({access_token})
 })
-
-server.use(/^(?!\/auth).*$/,  (req, res, next) => {
-  if (req.headers.authorization === undefined || req.headers.authorization.split(' ')[0] !== 'Bearer') {
-    const status = 401
-    const message = 'Error in authorization format'
-    res.status(status).json({status, message})
-    return
-  }
-  try {
-    let verifyTokenResult;
-     verifyTokenResult = verifyToken(req.headers.authorization.split(' ')[1]);
-
-     if (verifyTokenResult instanceof Error) {
-       const status = 401
-       const message = 'Access token not provided'
-       res.status(status).json({status, message})
-       return
-     }
-     next()
-  } catch (err) {
-    const status = 401
-    const message = 'Error access_token is revoked'
-    res.status(status).json({status, message})
-  }
-})
-
-server.use(router)
 
 server.listen(8000, () => {
   console.log('Run Auth API Server')
